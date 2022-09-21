ecf945a5728a47fddda2f4d2419bb9c601aec5eb
Muhammedabdelhey <73035247+Muhammedabdelhey@users.noreply.github.com>
2022-09-21 15:47:59 +0200

    sec version

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include './shared/header.php';
 include './shared/navbar.php';
 ?>
 <div >
-    <h1 class="text-center  pt-5"> Welecome to Homa page</h1>
+    <h1 class="text-center  pt-5"> Welecome to Home </h1>
 </div>
 <?php
 include './shared/footer.php';
