fa4913ddc55c718be9d120cbed7ca7d3d6178924
Muhammedabdelhey <73035247+Muhammedabdelhey@users.noreply.github.com>
2022-09-23 17:49:10 +0200

    second version

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@ include './shared/navbar.php';
 ?>
 <div class="container-fluid">
     
-    <img src="https://cdn.dribbble.com/users/1022481/screenshots/3018253/404-snow.gif" class="w-100 img-fluid" alt="">
+    <img src="https://weddingtonwitness.com/wp-content/uploads/2022/03/Fix-Error404-on-WordPress-780x400-1.jpg" class="w-100 img-fluid" alt="">
 </div>
 
 
